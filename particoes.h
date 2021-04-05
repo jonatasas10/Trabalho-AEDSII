161ce1568dbdf639b7a34d2b4174ff35309bdb14
Jonatas <jonatasas17@gmail.com>
2021-04-04 23:00:59 -0300

    Add files via upload

---
+++ particoes.h
@@ -0,0 +1,29 @@
+#ifndef PARTICOES_H
+#define PARTICOES_H
+
+#include <stdio.h>
+
+#include "lista.h"
+
+/* Algoritmo de geracao de particoes por Classificacao Interna, recebe como parâmetro
+o arquivo de dados de entrada, a lista contendo os nomes dos arquivos de saída das partições,
+o númer de elementos M a ser armazenado em cada partição e o número de registro total do arquivo.*/
+void classificacao_interna(FILE *arq, Lista *nome_arquivos_saida, int M, int nFunc);
+
+/*Algoritmo a ser implementado no trabalho de geracao de particoes por Selecao Natural recebe como parâmetro
+o arquivo de dados de entrada, a lista contendo os nomes dos arquivos de saída das partições,
+o númer de elementos M a ser armazenado em cada partição e o número de registro total do arquivo e o tamanho do reservatório.
+Fiquem a vontade para modificar a estrutura, foi uma sugestão.*/
+void selecao_natural(FILE *arq, Lista *nome_arquivos_saida, int M, int nFunc, int *qtd_particoes);
+
+TFunc *encontra_menor(TFunc **memoria, int M, int *posicao);
+
+void ordena (TFunc **v, int M);
+
+int reservatorio (TFunc *func, int contador, FILE *reservatorio, int M);
+
+void organiza_p(Lista* nome_arq, int qtd, int nFunc);
+
+void exibe (Lista *nome_arq, int qtd, int nFunc);
+
+#endif
